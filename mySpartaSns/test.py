852d355c394ea7415087bf3e0c5eef9174254eaa
HEUMMAN <whdudgma123@kpu.ac.kr>
2021-07-17 19:36:21 +0900

    스파르타 django 기초반 1주차
    스파르타 django 기초반 1주차

---
+++ mySpartaSns/test.py
@@ -0,0 +1,12 @@
+class project_02:
+    id = ''
+    title = ''
+    author = ''
+    content = ''
+
+homework = project_02()
+homework.id = 'id'
+homework.title = 'title'
+homework.author = 'heum'
+homework.content = 'content'
+

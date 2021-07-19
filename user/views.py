9faa15529926e11feb0d94f5eaacf7097aea71d7
HEUMMAN <whdudgma123@kpu.ac.kr>
2021-07-19 10:11:00 +0900

    회원가입, 로그인 기능 추가
    회원가입(중복ID 방지), 로그인(PW틀릴 시 다시 로그인 화면)기능 추가

--- user/views.py
+++ user/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import UserModel
+from django.http import HttpResponse
 
 def sign_up_view(request):
     if request.method == 'GET':
@@ -9,15 +10,29 @@ def sign_up_view(request):
         password = request.POST.get('password', None)
         password2 = request.POST.get('password2', None)
         bio = request.POST.get('bio', None)
-
         if password != password2:
-            return  render(request, 'user/signup.html')
+            return render(request, 'user/signup.html')
         else:
-            new_user = UserModel()
-            new_user.username = username
-            new_user.password = password
-            new_user.bio = bio
-            new_user.save()
-        return redirect('/sign-in')
+            exist_user = UserModel.objects.first(username=username)
+            if exist_user:
+                return render(request, 'user/signup.html')
+            else:
+                new_user = UserModel()
+                new_user.username = username
+                new_user.password = password
+                new_user.bio = bio
+                new_user.save()
+                return redirect('/sign-in')
 def sign_in_view(request):
-    return render(request, 'user/signin.html')
+    if request.method == 'POST':
+        username = request.POST.get('username', None)
+        password = request.POST.get('password', None)
+
+        me = UserModel.objects.get(username=username)
+        if me.password == password:
+            request.session['user'] = me.username
+            return HttpResponse(str(me.username)+"님 로그인 성공!")
+        else:
+            return  redirect('/sign-in')
+    elif request.method == 'GET':
+        return render(request, 'user/signin.html')

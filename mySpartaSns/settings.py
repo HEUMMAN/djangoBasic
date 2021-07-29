5bc70a3722c81ae1860c7b5bd2cbb50d7ef7f697
HEUMMAN <whdudgma123@kpu.ac.kr>
2021-07-30 01:57:58 +0900

    깃허브 올릴 준비
    깃허브 올릴 준비

--- mySpartaSns/settings.py
+++ mySpartaSns/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-b0uw$k0&7$8^ingj4@38zu^m&w&v-97cc5-m24)3)=p4g0^+39'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
